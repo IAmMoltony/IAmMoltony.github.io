a434afda2836dace202b5279d1075a4584c29ccf
IAmMoltony <koronavirusnyj@gmail.com>
2024-01-16 15:33:37 +0600

    Update mtnlog docs

--- mtnlog/docs/mtnlog_8h.js
+++ mtnlog/docs/mtnlog_8h.js
@@ -1,6 +1,7 @@
 var mtnlog_8h =
 [
     [ "mtnlogMessageC", "mtnlog_8h.html#a58aa7b023caa3a52cfb93a33e048a304", null ],
+    [ "mtnlogMessageTagC", "mtnlog_8h.html#a89a1a167ec9f4e04d7970012d1efbb17", null ],
     [ "MtnLogLevel", "mtnlog_8h.html#add77a7adbcdf0fd598df5ba21fb57177", [
       [ "MTNLOG_INFO", "mtnlog_8h.html#add77a7adbcdf0fd598df5ba21fb57177aba3ca7919450baca5d2d9699a9be141a", null ],
       [ "MTNLOG_WARNING", "mtnlog_8h.html#add77a7adbcdf0fd598df5ba21fb57177ad9e21e47518770cee49085a3d3d134e8", null ],
@@ -14,7 +15,9 @@ var mtnlog_8h =
     [ "mtnlogMessage", "mtnlog_8h.html#a6f82e4dc814f73a22638daf81c3b21ed", null ],
     [ "mtnlogMessageCInternal", "mtnlog_8h.html#a2630e29162297bdac65524bccb38474b", null ],
     [ "mtnlogMessageTag", "mtnlog_8h.html#a792a279d157d73c3d7ae2eb0d9dfa37a", null ],
+    [ "mtnlogMessageTagCInternal", "mtnlog_8h.html#a966b3bdc1ac98bdb456328a2b835ceaf", null ],
     [ "mtnlogSetLevel", "mtnlog_8h.html#aee8f2cb1f38b72f9fd22f9ed49353a78", null ],
     [ "mtnlogTimestamps", "mtnlog_8h.html#a4260c51f83ab81c73bbb34907e3ba231", null ],
-    [ "mtnlogVMessage", "mtnlog_8h.html#ad6a803016f57b67823d11c832c504edd", null ]
+    [ "mtnlogVMessage", "mtnlog_8h.html#ad6a803016f57b67823d11c832c504edd", null ],
+    [ "mtnlogVMessageTag", "mtnlog_8h.html#aa3ecea232d5927e4c2107f0d72899663", null ]
 ];
