83e4f64a790a07ba06ef30a2f4a61240a3ffa8c8
IAmMoltony <koronavirusnyj@gmail.com>
2024-01-30 21:20:02 +0600

    Update mtnlog docs

--- mtnlog/docs/menudata.js
+++ mtnlog/docs/menudata.js
@@ -29,6 +29,7 @@ var menudata={children:[
 {text:"Globals",url:"globals.html",children:[
 {text:"All",url:"globals.html"},
 {text:"Functions",url:"globals_func.html"},
+{text:"Typedefs",url:"globals_type.html"},
 {text:"Enumerations",url:"globals_enum.html"},
 {text:"Enumerator",url:"globals_eval.html"},
 {text:"Macros",url:"globals_defs.html"}]}]}]}
