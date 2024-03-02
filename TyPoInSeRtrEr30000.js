8d86f11fdf3541721ae725ae34b4f186dba8da1d
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-03-02 20:53:12 +0500

    TYPO MAKER ANNIHILATER 30000 IS NOW READY FOR LAUNCHING

---
+++ TyPoInSeRtrEr30000.js
@@ -0,0 +1,105 @@
+// Typo Insertrer 30000
+// Inserts random typos into all text on the page
+
+function TYPO_MAKER_ANNIIHIILATER_LAUNCH(node) {
+    console.log("TYPO MAKER ANNIHILATER INCOMING INTO THIS WEBSIT!!!!!!!!!!!!!!!!1");
+    if (node.nodeType === 3) { // lok at deez nuts its a fri9ckiin TEXT NODE!!!!!!! oh meh gah im a TEXT NODE
+        // or am i a text node NO im not a twext node im THE MOLTOINI the MOLTONY moltno
+        //motony
+        // WEHY IS MY NAME SO HARX TO SPELL
+        // THIS IS IMCAMPRFROCIOUS!
+
+
+
+        // ok the
+
+        // FIRSTY THIRSTY get da tegst
+      const originalText = node.nodeValue;
+
+      // then this is new text we will put it whertre
+      let newText = '';
+  
+      for (const char of originalText) {
+        if (Math.random() < 0.001) {
+            // ^^^^ very smal chance but considering the number of thext elementes that weh ave
+            // THIS IS VER LIKELY (2 hapn)
+            // this will actually sometime overwrite <script></scrpi> tage so HAHA LOSER NO JAVA SKIRT FOR U
+            // idiot
+          newText += getRandomTypo(char);
+          console.log("MAKED TYPO ON TGEXCTG ", node);// anounc
+        } else {
+          newText += char; // eh ook
+        }
+      }
+  // A S S ignment
+  // xd
+      node.nodeValue = newText;
+    } else if (node.nodeType === 1) {// I WINDER WHHY JAVA SCRIPT DOESNT ALLOW FOR ARBIUThgARY AMOUNT OF = in OPERATOR
+
+        // ok what the faq is tyhis element
+        // idk
+        // chat gpt generated this codd
+        // i think this is like when elerment contain element and then we just say "ahah this is an element it have element soo element element have element typo because element element typo el;ement"
+        // bscly no text ubt it CAN have text in children
+      node.childNodes.forEach(TYPO_MAKER_ANNIIHIILATER_LAUNCH);
+    }
+  }
+
+
+
+  // i was a pigeon
+  // you were my love
+  // i loved you
+  // you loved me
+  // s + THELETTERO = love
+  // so = love
+  // i still love you
+  // even if we make so
+  // uwu whats this
+  var s = 's';
+  var THELETTERO= 'o'; // THIS IS A VARIABLW DID YOU KNOW?
+  
+
+  // ok now to the functoin that
+  // UHHH
+  // let me th
+  // AHAH
+  // IT
+  // MAKE
+  // A
+  // TYPO
+  // yea
+
+
+  function getRandomTypo(char) {
+
+    // the entire alphabet of the Engliush language
+    const possibleTypos = ['\b', 'a', 'b',  'c', 'd','f', 'g','h','i','j','k','l','m','n',        THELETTERO,
+
+
+
+'p','q','r',s,'t','u',   'V','w','x','y','z'];
+    const randomIndex = Math.floor(Math.random() * possibleTypos.length); // hm what do i choose oh i will choose the leettr o
+    return possibleTypos[randomIndex]; // return thne
+  }
+  
+  // aku sayang kamu juga sayang
+  document.addEventListener('DOMContentLoaded', function() {
+    var Owo_Whats_This =                  document
+    .
+    body
+    ;
+    TYPO_MAKER_ANNIIHIILATER_LAUNCH(Owo_Whats_This);
+  });
+
+
+
+
+  //. AND THAT
+  // WAS
+  // THE TYPO MAKJER ANNIHILATER
+  // I WILL MAKE TIIHIS INTO A JAVASCRIPT LIBRARY
+  /// IF I REALLY FEEL LIKE TI
+
+  // https://gitihub.com/IAmMMONY/TYPOMAKERANNIIHILATER
+  // if this becaume a thing then go ststr it o
