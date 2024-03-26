505350c19a62bc58e957720d3889d6e0ebb205d5
IAmMoltony <koronavirusnyj@gmail.com>
2024-03-26 16:52:44 +0500

    TYPO INSERTER 30000000000 VERSION 2 !!!!!!!!!!!!!!!!!!!!!!!
    TYPO INSERTER VERSION 2 IS EVEN MORE EFFICIENT IN ANNIHILATERING the WEB PAGE with the ALL NEW ASS MODE that MAKES THE WEB PAGE OF THE WEBSITE UNREADABEL

--- TyPoInSeRtrEr30000.js
+++ TyPoInSeRtrEr30000.js
@@ -1,8 +1,46 @@
 // Typo Insertrer 30000
 // Inserts random typos into all text on the page
+//
+//ANIHILATER VERSION 2
+//ADDED ASS MODE
+//AND MORE TYPO CHANCES
+//AND THE SPACE CHAR IN THE TYPO
+//
+
+let PopOsCosmicDesktop = ' ';
+function jfjgjjifewjefjfejowjifjowjfewjif9ewfwjiewfjoefwoifjoiejef(){
+
+
+alert("warning: ASS MODE is enabled");
+}
+
+const FORCE_ASS_MODE = !true; // this is wher tr to enable ASS MODE by defaul (Xxterminly DANGEROUSSY)
+
+    //
+    // ASS MODE
+    // IF ASS MODE ENABLES THEN THE ENTIRE PAHE GETS TURNED INTO A RUSHED ESSYA
+
+    const ASS_MODE = Math.random() < 0.001 || FORCE_ASS_MODE;
+
+    // if ASS MODE is enabl then go THE FJFJFJ
+    const typoFrequency = ASS_MODE ? 0.8 : 0.01;
+
+
+    // anounce ASS MODE
+// lol the fi statemtn look like a old granpa
+    if (ASS_MODE)
+{
+    for (let i = 0; i < 100; i++)
+        console.log(i == 0 ? "" : "I REPEAT!!!l" + "CEMENT: ASS MODE IS wa ENEBLAE FOR THIS SESSION, SEEK SHELT ER IMEDITALYE!");
+    jfjgjjifewjefjfejowjifjowjfewjif9ewfwjiewfjoefwoifjoiejef();
+}
+    else
+        // announe LACK of ASS MODE
+        console.log("Cement: ASS MODE is diable fo rthisj ession. NIc e!");
 
 function TYPO_MAKER_ANNIIHIILATER_LAUNCH(node) {
     console.log("TYPO MAKER ANNIHILATER INCOMING INTO THIS WEBSIT!!!!!!!!!!!!!!!!1"); // anounc that WE are TYPO MAKERING ANIHIALTERING THE WEB PAGE
+
     if (node.nodeType === 3) { // lok at deez nuts its a fri9ckiin TEXT NODE!!!!!!! oh meh gah im a TEXT NODE
         // or am i a text node NO im not a twext node im THE MOLTOINI the MOLTONY moltno
         //motony
@@ -20,11 +58,13 @@ function TYPO_MAKER_ANNIIHIILATER_LAUNCH(node) {
       let newText = '';
   
       for (const char of originalText) {
-        if (Math.random() < 0.001) {
+        if (Math.random() < typoFrequency) {
             // ^^^^ very smal chance but considering the number of thext elementes that weh ave
             // THIS IS VER LIKELY (2 hapn)
             // this will actually sometime overwrite <script></scrpi> tage so HAHA LOSER NO JAVA SKIRT FOR U
             // idiot
+            // upday on th <skirt></skit> situation I THINK IT STILL WORK after ANINIHLATERING but dw fr rn tbh idc
+            // also if the enabled AS MODE thn PAI PAI GRAMMAFR!!!!!!!!!111111111111j1jo1j11j!!joi eheheh
           newText += getRandomTypo();
           console.log("MAKED TYPO ON TGEXCTG ", node);// anounc
         } else {
@@ -74,11 +114,18 @@ function TYPO_MAKER_ANNIIHIILATER_LAUNCH(node) {
   function getRandomTypo() {
 
     // the entire alphabet of the Engliush language
-    const possibleTypos = ['\b', 'a', 'b',  'c', 'd','f', 'g','h','i','j','k','l','m','n',        THELETTERO,
+    var possibleTypos = ['\b', 'a', 'b',  'c', 'd','f', 'g','h','i','j','k','l','m','n',        THELETTERO,
 
 
 
-'p','q','r',s,'t','u',   'V','w','x','y','z'];
+'p','q','r',s,'t','u',   'V','w','x','y','z',      PopOsCosmicDesktop,
+    ];
+      for (var saYang = 1; saYang < 1024; saYang ++ ) { 
+                                                    {
+                                                                            // add
+                                                        possibleTypos.push(String.fromCharCode(saYang));
+                                                    }
+      }
     const randomIndex = Math.floor(Math.random() * possibleTypos.length); // hm what do i choose oh i will choose the leettr o
     return possibleTypos[randomIndex]; // return thne
   }
