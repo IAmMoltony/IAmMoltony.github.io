9c351c256b2a519342533fc606cef9e29f3b7b8e
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-14 16:46:18 +0500

    Update MtnLog docs to 2.7.2

--- mtnlog/docs/dir_d44c64559bbebec7f509842c48db8b23.js
+++ mtnlog/docs/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -1,5 +1,5 @@
 var dir_d44c64559bbebec7f509842c48db8b23 =
 [
-    [ "mtnlog.h", "mtnlog_8h_source.html", null ],
+    [ "mtnlog.h", "mtnlog_8h.html", "mtnlog_8h" ],
     [ "mtnlogversion.h", "mtnlogversion_8h.html", "mtnlogversion_8h" ]
 ];
--- mtnlog/docs/mtnlog_8h.js
+++ mtnlog/docs/mtnlog_8h.js
@@ -1,5 +1,6 @@
 var mtnlog_8h =
 [
+    [ "MTNLOG_BOOL", "mtnlog_8h.html#a13b389ddd0ffe1ef934130995bf457fc", null ],
     [ "mtnlogMessageC", "mtnlog_8h.html#a58aa7b023caa3a52cfb93a33e048a304", null ],
     [ "mtnlogMessageTagC", "mtnlog_8h.html#a89a1a167ec9f4e04d7970012d1efbb17", null ],
     [ "MtnLogCallback", "mtnlog_8h.html#a384d7f815022797906c46ec560fe8c73", null ],
@@ -8,11 +9,11 @@ var mtnlog_8h =
       [ "MTNLOG_WARNING", "mtnlog_8h.html#add77a7adbcdf0fd598df5ba21fb57177ad9e21e47518770cee49085a3d3d134e8", null ],
       [ "MTNLOG_ERROR", "mtnlog_8h.html#add77a7adbcdf0fd598df5ba21fb57177a570f1726bcaff81cce3292608b0eb406", null ]
     ] ],
-    [ "mtnlogCheckColor", "mtnlog_8h.html#a8fb0baa85bd4095257d0d545199f7d7b", null ],
-    [ "mtnlogColor", "mtnlog_8h.html#a962a8b961c369e4b806a21ae5399fb56", null ],
-    [ "mtnlogConsoleOutput", "mtnlog_8h.html#ae00fb129bf4f486bd96439b07d208d89", null ],
-    [ "mtnlogConsoleTimestamps", "mtnlog_8h.html#ab1ecc464995b740c5b5540eff3c81c6d", null ],
-    [ "mtnlogFileOutput", "mtnlog_8h.html#acdd53560d3647c8a41106595792bd106", null ],
+    [ "mtnlogCheckColor", "mtnlog_8h.html#a71eb540d0b5f2421e11de69369084d0b", null ],
+    [ "mtnlogColor", "mtnlog_8h.html#ac10c9ffd014337834a0f99b0f5da45d3", null ],
+    [ "mtnlogConsoleOutput", "mtnlog_8h.html#ab57afa4a01d73a3e725415de743d2e6c", null ],
+    [ "mtnlogConsoleTimestamps", "mtnlog_8h.html#ae5627baf3c065512af7841638a369af1", null ],
+    [ "mtnlogFileOutput", "mtnlog_8h.html#abc50b64b27121207fc919f8f893e2145", null ],
     [ "mtnlogInit", "mtnlog_8h.html#aca99dccb03f66303bcfd59333bf048ba", null ],
     [ "mtnlogMessage", "mtnlog_8h.html#a6f82e4dc814f73a22638daf81c3b21ed", null ],
     [ "mtnlogMessageCInternal", "mtnlog_8h.html#a2630e29162297bdac65524bccb38474b", null ],
@@ -20,7 +21,7 @@ var mtnlog_8h =
     [ "mtnlogMessageTagCInternal", "mtnlog_8h.html#a966b3bdc1ac98bdb456328a2b835ceaf", null ],
     [ "mtnlogSetCallback", "mtnlog_8h.html#aa0ac08244e52826e41dbedf8c1742db9", null ],
     [ "mtnlogSetLevel", "mtnlog_8h.html#aee8f2cb1f38b72f9fd22f9ed49353a78", null ],
-    [ "mtnlogTimestamps", "mtnlog_8h.html#a4260c51f83ab81c73bbb34907e3ba231", null ],
+    [ "mtnlogTimestamps", "mtnlog_8h.html#aaa42b9b2fd72e3433142dff0f09379c9", null ],
     [ "mtnlogVMessage", "mtnlog_8h.html#ad6a803016f57b67823d11c832c504edd", null ],
     [ "mtnlogVMessageTag", "mtnlog_8h.html#aa3ecea232d5927e4c2107f0d72899663", null ]
 ];
