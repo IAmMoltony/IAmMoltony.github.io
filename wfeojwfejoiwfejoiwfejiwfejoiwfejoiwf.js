a08ae189b795409b87119c8bf9f565a4b0e46837
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-04-19 22:25:55 +0500

    I already have Comic Sans installed on my system, dear Moltony's Awsome Website 6942013379000.

---
+++ wfeojwfejoiwfejoiwfejiwfejoiwfejoiwf.js
@@ -0,0 +1,4 @@
+if (localStorage.getItem("IALREADYHAVECOMICSANSSOSHUTUP") == "ok i shut up")
+    // Hide the bodies
+    document.querySelector("#cococococococococo").style.display = //'block' ;
+    'none';
